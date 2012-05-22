3f4a9deb9c1ffcb3e002e053e1000b57d5c37b37
welsch <devnull@localhost>
2012-05-22 15:50:39 +0200

    converted to original Sieber & Meyer 1988 example

--- libraries/cb/new/cb/A.java
+++ libraries/cb/new/cb/A.java
@@ -2,9 +2,9 @@ package cb;
 
 public class A {
   private int g;
-  public A exec(C c) {
+  public boolean exec(C c) {
     if (c != null) c.run();
-    return null;
+    return true;
   }
   public void inc() { g = g + 2; }
 }
--- libraries/cb/old/cb/A.java
+++ libraries/cb/old/cb/A.java
@@ -2,10 +2,9 @@ package cb;
 
 public class A {
   private int g;
-  public A exec(C c) {
+  public boolean exec(C c) {
     if (c != null) c.run();
-    if (g % 2 == 0) { return null; }
-    else { return this; }
+    return g % 2 == 0;
   }
   public void inc() { g = g + 2; }
 }
