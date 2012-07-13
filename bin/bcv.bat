86da917e30997b183cb9820f2b01895023ad4c27
welsch <devnull@localhost>
2012-07-13 14:29:30 +0200

    added README file, removed POM file as all functionality now implemented in gradle file

--- bin/bcv.bat
+++
@@ -1,1 +0,0 @@
-mvn -q -f "%~dp0..\pom.xml" exec:java -Dexec.mainClass="de.unikl.bcverifier.Main" -Dexec.args="%*"
--- bin/bcvdir.bat
+++
@@ -1,3 +0,0 @@
-set _FIRSTARG=%~1
-shift
-mvn -q -f "%~dp0..\pom.xml" exec:java -Dexec.mainClass="de.unikl.bcverifier.Main" -Dexec.args="-l %_FIRSTARG%\old %_FIRSTARG%\new -i %_FIRSTARG%\bpl\inv.bpl %1 %2 %3 %4 %5 %6 %7 %8 %9"
