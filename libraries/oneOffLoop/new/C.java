3b83a24f5485214cfd163a5ee390f46ece06bf96
mweber <devnull@localhost>
2012-07-18 16:38:13 +0200

    new example oneOffLoop using conditional places

---
+++ libraries/oneOffLoop/new/C.java
@@ -0,0 +1,13 @@
+public class C{
+	private MyList list;
+
+	public int m(int n){
+		int x = 0;
+		int i = 1;
+		while(i<n){
+			x += i;
+			i++;
+		}
+		return x;
+	}
+}
---
+++ libraries/oneOffLoop/old/C.java
@@ -0,0 +1,11 @@
+public class C{
+	private MyList list;
+
+	public int m(int n){
+		int x = 0;
+		for(int i=0; i<n; i++){
+			x += i;
+		}
+		return x;
+	}
+}
