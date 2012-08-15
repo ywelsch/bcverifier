12a4ea7e047bad4bd61d18322fdd680e4a303f21
welsch <devnull@localhost>
2012-08-15 15:36:19 +0200

    Formatting as for other exaples (indent 2)

--- libraries/cell/new/cell/Cell.java
+++ libraries/cell/new/cell/Cell.java
@@ -1,13 +1,13 @@
 package cell;
 
 public class Cell<T> {
-    private T c1, c2;
-    private boolean f;
-    public void set(T o) {
-        f = !f;
-        if(f) c1 = o; else c2 = o;
-    }
-    public T get(){
-        return f ? c1 : c2;
-    }
+  private T c1, c2;
+  private boolean f;
+  public void set(T o) {
+    f = !f;
+    if(f) c1 = o; else c2 = o;
+  }
+  public T get(){
+    return f ? c1 : c2;
+  }
 }
--- libraries/cell/old/cell/Cell.java
+++ libraries/cell/old/cell/Cell.java
@@ -1,7 +1,7 @@
 package cell;
 
 public class Cell<T> {
-    private T c;
-    public void set(T o) {c = o;}
-    public T get() {return c;}
+  private T c;
+  public void set(T o) {c = o;}
+  public T get() {return c;}
 }
