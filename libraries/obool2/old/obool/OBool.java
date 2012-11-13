9743d6b448086eb45b3c820793b9868dd86136d3
welsch <devnull@localhost>
2012-11-13 12:01:09 +0100

    forgot to add 2 files

---
+++ libraries/obool2/old/obool/OBool.java
@@ -0,0 +1,19 @@
+package obool;
+
+public class OBool {
+  private Bool g = new Bool();
+  public OBool() { 
+	g.set(false);
+  }
+  public void setg(boolean b) { 
+	g.set(b); 
+  }
+  public boolean getg() { 
+	return g.get();
+  }
+  public OBool clone() {
+	OBool clone = new OBool();
+	clone.g = g;
+	return clone;
+  }
+}
