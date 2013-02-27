8341557baedde239d18147e116c9f34b732e8f1f
Peter Zeller <p_zeller@cs.uni-kl.de>
2013-02-27 15:17:34 +0100

    removed ackermann testfile

--- examples/ackermann/old/ackermann/Ackermann.java
+++ examples/ackermann/old/ackermann/Ackermann.java
@@ -4,12 +4,17 @@ public class Ackermann {
 	public static int a(int x, int y) {
 		if (x < 0 || y < 0) 
 			return 0;
+		int res;
 		if (x == 0) {
-			return y+1;
+			res = y+1;
+			return res;
 		} else if (y == 0) {
-			return a(x-1, 1);
+			res = a(x-1, 1);
+			return res;
 		} else {
-			return a(x-1, a(x,y-1));
+			int r = a(x,y-1);
+			res = a(x-1, r);
+			return res;
 		}
 	}
 }
